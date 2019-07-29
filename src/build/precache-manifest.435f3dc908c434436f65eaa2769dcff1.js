efbbfe2f51ae3c9c562f10d9971da6b1adb8de22
Lee Kempton <lee@loaded.games>
2019-07-29 14:29:32 +0100

    First Commit

---
+++ src/build/precache-manifest.435f3dc908c434436f65eaa2769dcff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77ee1dadd297756d0af81b6b39c9516",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ecf1347f6f9bc03ca03",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "3ecf1347f6f9bc03ca03",
+    "url": "/static/js/main.d7072c55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
